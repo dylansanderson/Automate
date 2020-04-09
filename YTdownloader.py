1cbbffa89829d2c3d6102eb19f1e0a2bc8946ad5
Dylan <dylansanderson@csu.fullerton.edu>
2020-04-08 21:41:15 -0700

    Update YTdownloader.py

--- YTdownloader.py
+++ YTdownloader.py
@@ -5,6 +5,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 """ 
+YOUTUBE VIDEO DOWNLOADER, USING YOUTUBETOMP3
 1) input song into yt search bar
 2) click on first result
 3 copy current url, then driver.get("(downloader website)")
