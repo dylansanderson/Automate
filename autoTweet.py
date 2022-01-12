9283d10a2bff474f4af6f65fc40fd64a166fd031
Dylan <dylansanderson@csu.fullerton.edu>
2022-01-12 04:34:47 -0800

    Update autoTweet.py
    adjust for new UI

--- autoTweet.py
+++ autoTweet.py
@@ -12,20 +12,28 @@ class Twitter:
         self.driver = webdriver.Chrome(executable_path=r"C:\Users\dylan\Desktop\chromedriver.exe") #path to your chrome driver
         self.driver.maximize_window()
     def login(self):
-        self.driver.get("https://twitter.com")
+        self.driver.get("https://twitter.com/i/flow/login")
         sleep(2)
-        login = self.driver.find_element_by_xpath('//*[@id="react-root"]/div/div/div/main/div/div/div/div[1]/div/a[2]').click()             
-        emailInputBox = self.driver.find_element_by_xpath('//*[@id="react-root"]/div/div/div[2]/main/div/div/div[1]/form/div/div[1]/label/div/div[2]/div/input')
-        emailInputBox.send_keys("email")
-        passwordInputBox = self.driver.find_element_by_xpath('//*[@id="react-root"]/div/div/div[2]/main/div/div/div[1]/form/div/div[2]/label/div/div[2]/div/input')
-        passwordInputBox.send_keys("password")
-        loginButton = self.driver.find_element_by_xpath('//*[@id="react-root"]/div/div/div[2]/main/div/div/div[1]/form/div/div[3]/div')
-        loginButton.click()
+               
+        emailInputBox = self.driver.find_element_by_xpath('//*[@id="layers"]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div[5]/label/div/div[2]/div/input')
+        emailInputBox.send_keys("someEmail@somedomain.com")
+        nextBTN = self.driver.find_element_by_xpath('//*[@id="layers"]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div[6]/div').click()
+        sleep(1)
+        try:
+            PWentry = self.driver.find_element_by_xpath('//*[@id="layers"]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div[3]/div/label/div/div[2]/div[1]/input').send_keys("YOURPASSWORD")
+            loginBTN = self.driver.find_element_by_xpath('//*[@id="layers"]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[2]/div/div/div').click()
+            sleep(1)
+        except Exception: #exception if prompted for extra verification.. input phone number
+            phoneInput = self.driver.find_element_by_xpath('//*[@id="layers"]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div[2]/label/div/div[2]/div/input').send_keys("XXX-XXX-XXXX")
+            nextBTN = self.driver.find_element_by_xpath('//*[@id="layers"]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[2]/div/div').click()
+            sleep(2)
+            PWentry = self.driver.find_element_by_xpath('//*[@id="layers"]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div[3]/div/label/div/div[2]/div[1]/input').send_keys("Tranent999!")
+            loginBTN = self.driver.find_element_by_xpath('//*[@id="layers"]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[2]/div/div/div').click()
         sleep(2)
     def send_tweet(self):
-        textInput = self.driver.find_element_by_xpath('//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div/div/div[2]/div/div[2]/div[1]/div/div/div/div[2]/div[1]/div/div/div/div/div/div/div/div/div/div[1]/div/div/div/div[2]/div/div/div/div')
-        textInput.send_keys(str(datetime.datetime.now()))
-        tweetButton = self.driver.find_element_by_xpath('//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div/div/div[2]/div/div[2]/div[1]/div/div/div/div[2]/div[4]/div/div/div[2]/div[3]')
+        textInput = self.driver.find_element_by_xpath('//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[1]/div/div[2]/div/div[2]/div[1]/div/div/div/div[2]/div[1]/div/div/div/div/div/div/div/div/div/label/div[1]/div/div/div/div/div[2]/div/div/div/div')
+        textInput.send_keys("The Current time is: " + str(datetime.datetime.now()))
+        tweetButton = self.driver.find_element_by_xpath('//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[1]/div/div[2]/div/div[2]/div[1]/div/div/div/div[2]/div[3]/div/div/div[2]/div[3]/div/span/span')
         tweetButton.click()
         sleep(1)
        
@@ -37,5 +45,5 @@ t = Twitter()
 t.login()
 while True:        
     t.send_tweet()
-    sleep(random.randint(120,280))            # Give time in between tweets to avoid Twitter's spam detection 
+    sleep(random.randint(200,1500))            # Give time in between tweets to avoid Twitter's spam detection 
 t.quit()
